a4f24ee267bc9217e2fe30df978094ac7f72f249
Agent Diamonds <z.sorensen1@gmail.com>
2024-02-03 05:44:59 -0700

    add basic test code

---
+++ src/BetterGovernors/BetterGovernors.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.Core;
+using TaleWorlds.Library;
+using TaleWorlds.Localization;
+using TaleWorlds.MountAndBlade;
+
+
+namespace BetterGovernors
+{
+    public class BetterGovernors : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            Module.CurrentModule.AddInitialStateOption(new InitialStateOption("Better Governors Test",
+                new TextObject("Better Governors Test", null),
+                9990,
+                () => { InformationManager.DisplayMessage(new InformationMessage("Better Governors Test Successful!")); },
+                () => { return (false, null); }));
+        }
+    }
+}
